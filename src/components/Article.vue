0df748c4d755000df2e739410b4ca09987b2ca72
Terry Moore II <terry.moore.ii@gmail.com>
2017-09-24 16:59:18 -0400

    style cleanup

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,25 +1,24 @@
 <template>
   <article class="media has-shadow">
-      <vd-vote :article="article"></vd-vote>
+    <vd-vote :article="article"></vd-vote>
     <div class="media-content">
       <div class="content">
         <p>
-          <strong class="is-size-5"><a :href="article.url">{{article.title}}</a></strong> <small><em><a class="has-text-grey-dark" :href="article.url">({{getHostName}})</a></em></small>
-          <br>
-          <span class="is-size-6">
-            <small class="has-text-grey">
-              <span class="has-text-grey-dark">{{ article.submittedBy }}</span> <em :title="article.submittedDate | toDateString">{{ article.submittedDate | timeago }}</em>
-            </small>
-          </span>
-          <br>
-          <router-link class="has-text-grey" :to="{ name: 'comments', params: { id: article._id }}" v-if="article._id && !hideCommentLink">
-            {{article.commentCount || 0}} Comments
-          </router-link>
-          <a class="twitter-share-button has-text-grey" target="_blank"
-               :href="twitterUrl">
-            <span class="icon is-small"><i class="fa fa-twitter"></i></span>&nbspTweet
-          </a>
+          <strong class="is-size-5"><a :href="article.url">{{article.title}}</a></strong>
+          <small><em><a class="has-text-grey-dark" :href="article.url">({{getHostName}})</a></em></small>
         </p>
+        <p class="info">
+          <span class="has-text-grey-dark">{{article.submittedBy}}</span>
+          <small><em class="has-text-grey">{{ article.submittedDate | timeago }}</em></small>
+          <span class="comment" v-if="article._id && !hideCommentLink">
+            <router-link class="has-text-grey"
+                         :to="{ name: 'comments', params: { id: article._id }}">{{article.commentCount || 0}} Comments</router-link>
+          </span>
+        </p>
+        <!--<a class="twitter-share-button has-text-grey" target="_blank"-->
+        <!--:href="twitterUrl">-->
+        <!--<span class="icon is-small"><i class="fa fa-twitter"></i></span>&nbspTweet-->
+        <!--</a>-->
       </div>
       <div>
         <span class="tag is-primary is-capitalized" v-for="tag in article.tags">{{tag}}</span>
@@ -29,59 +28,84 @@
 </template>
 
 <script>
-import VdVote from './Vote.vue';
+  import VdVote from './Vote.vue';
 
-export default {
-  name: 'VdArticle',
-  components: {
-    'vd-vote': VdVote
-  },
-  props: {
-    article: {
-      type: Object | String,
-      required: true
+  export default {
+    name: 'VdArticle',
+    components: {
+      'vd-vote': VdVote
     },
-    hideCommentLink: {
-      type: Boolean,
-      required: false
-    }
-  },
-  filters: {
-    toDateString (date) {
-      return new Date(date).toLocaleString();
-    }
-  },
-  methods: {
-  },
-  computed: {
-    twitterUrl () {
-      return `https://twitter.com/intent/tweet?text=${this.article.title} ${this.article.url} @vuedocs`;
+    props: {
+      article: {
+        type: Object | String,
+        required: true
+      },
+      hideCommentLink: {
+        type: Boolean,
+        required: false
+      }
     },
-    getHostName () {
-      if (!this.article || !this.article.url) {
-        return '';
+    filters: {
+      toDateString (date) {
+        return new Date(date).toLocaleString();
       }
-      let hostname;
-      let url = this.article.url;
+    },
+    methods: {},
+    computed: {
+      twitterUrl () {
+        return `https://twitter.com/intent/tweet?text=${this.article.title} ${this.article.url} @vuedocs`;
+      },
+      getHostName () {
+        if (!this.article || !this.article.url) {
+          return '';
+        }
+        let hostname;
+        let url = this.article.url;
 
-      if (url.indexOf('://') > -1) {
-        hostname = url.split('/')[2];
-      } else {
-        hostname = url.split('/')[0];
+        if (url.indexOf('://') > -1) {
+          hostname = url.split('/')[2];
+        } else {
+          hostname = url.split('/')[0];
+        }
+
+        hostname = hostname.split(':')[0];
+        hostname = hostname.split('?')[0];
+        hostname = hostname.replace('www.', '');
+
+        return hostname;
       }
-
-      hostname = hostname.split(':')[0];
-      hostname = hostname.split('?')[0];
-      hostname = hostname.replace('www.', '');
-
-      return hostname;
     }
-  }
-};
+  };
 </script>
 
 
 <style scoped>
-  .tag{ margin-left: 4px }
-  .twitter-share-button{ margin-left: 4px }
+  .tag {
+    margin-left: 4px
+  }
+
+  .twitter-share-button {
+    margin-left: 4px
+  }
+
+  p {
+    margin-bottom: 0 !important;
+  }
+
+  .info {
+    margin-top: 4px;
+  }
+
+  .comment a {
+    padding-left: 4px;
+  }
+
+  .comment:before {
+    content: '|';
+    color: #777;
+  }
+
+  .comment a:hover {
+    text-decoration: underline;
+  }
 </style>
