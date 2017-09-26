6b13e13ff2d638cc1692c22acb68c70cb9986744
Terry Moore II <terry.moore.ii@gmail.com>
2017-09-25 20:37:01 -0400

    Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="isLoading" class="loading-overlay flex-container">
+    <div class="flex-item">
+      <i class="fa fa-circle-o-notch fa-3x fa-spin"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from 'Vuex';
+
+  export default {
+    name: 'VdLoading',
+    watch: {
+      isLoading (val) {
+        console.log('loading', val);
+      }
+    },
+    computed: {
+      ...mapGetters({isLoading: 'isLoading'})
+    }
+  };
+</script>
+
+
+<style scoped>
+  .loading-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background-color: rgba(255, 255, 255, 0.6);
+  }
+
+  .flex-container {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .flex-item {
+    text-align: center;
+  }
+
+</style>
---
+++ src/store/modules/loading.js
@@ -0,0 +1,34 @@
+import * as types from '../mutation-types';
+
+// initial state
+const state = {
+  isLoading: null
+};
+
+// getters
+const getters = {
+  isLoading: state => state.show
+};
+
+// actions
+const actions = {
+  // setUser ({ commit }) {
+  //   shop.getProducts(products => {
+  //     commit(types.RECEIVE_PRODUCTS, { products })
+  //   })
+  // }
+};
+
+// mutations
+const mutations = {
+  [types.LOADING] (state, isLoading) {
+    state.isLoading = isLoading;
+  }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
